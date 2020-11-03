96d7f65b5f2bee06dfa850ed79929b48ffca69d8
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2020-11-03 15:33:48 +0100

    Home Page: added style

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+export const HomeContainer = styled.div`
+  margin: 0 auto;
+  max-width: 780px;
+  padding: 10vh 5vw;
+
+  place-content: center;
+
+  display: flex;
+  flex-direction: column;
+
+  background-color: lightblue;
+
+  h1 {
+    font-size: 2rem;
+    text-align: center;
+    color: #303EA0;
+    margin-bottom: 5vh;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+
+    div {
+      & + div {
+        margin-top: 4vh;
+      }
+    }
+
+    p {
+      font-size: 1rem;
+      margin-top: 4vh;
+
+      color: #ff6655;
+    }
+
+    button {
+      margin-top: 4vh;
+    }
+  }
+`;
