c44eb0266a08db6a7f66c46d946bfa072b440503
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2020-11-03 16:54:46 +0100

    CheckinConfirmed Page: added styles

---
+++ src/pages/CheckinConfirmed/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const CheckinContainer = styled.div`
+  margin: 0 auto;
+  max-width: 780px;
+  min-height: 100vh;
+  padding: 10vh 5vw;
+
+  place-content: center;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  background-color: lightblue;
+
+  h1 {
+    font-size: 2rem;
+    text-align: center;
+    color: #303EA0;
+    margin-bottom: 5vh;
+  }
+
+  a {
+    margin-top: 4vh;
+  }
+`;
