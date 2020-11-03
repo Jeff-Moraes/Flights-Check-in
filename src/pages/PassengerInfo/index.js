00364e681e54864d1123efd0c6a3b3722338ae7a
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2020-11-03 01:10:33 +0100

    PassengerInfo Page: created PassengerInfo page and route

---
+++ src/pages/PassengerInfo/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PassengerInfo({ lastName }) {
+
+  return (
+    <div>
+      <h1>Welcome, {lastName}</h1>
+    </div>
+  );
+}
+
+export default PassengerInfo;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 import Home from "../pages/Home";
+import PassengerInfo from "../pages/PassengerInfo";
 
 const Routes = () => {
   const [ flightNumber, setFlightNumber ] = useState("");
+  const [ lastName, setLastName ] = useState("");
   const [ passengerIndex, setPassengerIndex ] = useState(null);
 
   return (
@@ -16,8 +18,22 @@ const Routes = () => {
           <Home {...props}
             flightNumber={flightNumber}
             setFlightNumber={setFlightNumber}
+            lastName={lastName}
+            setLastName={setLastName}
             setPassengerIndex={setPassengerIndex}
           />)}
+      />
+      <Route
+        path="/passenger-info"
+        exact
+        render={(props) => {
+          if (passengerIndex !== null) {
+              return <PassengerInfo {...props} lastName={lastName} />;
+            } else {
+              return <Redirect to="/" />;
+            }
+          }
+        }
       />
     </Switch>
   );
